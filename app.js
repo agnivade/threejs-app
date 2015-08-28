4849e7be1c0c76a039afed49ac1269bef820e81f
Agniva De Sarker <agnivade@yahoo.co.in>
2015-08-28 20:44:09 +0530

    Updating code so that it casts a shadow

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ function init() {
   renderer.shadowMapEnabled = true;
   //renderer.shadowMapSoft = true;
   renderer.setSize(WIDTH, HEIGHT);
+  renderer.setClearColor(0x333F47, // color
+                          1); // opacity
   document.body.appendChild(renderer.domElement);
 
   //create a camera
@@ -24,30 +26,38 @@ function init() {
                             WIDTH / HEIGHT, //  Aspect ratio
                             0.1, // Near distance
                             10000); // Far distance
-  camera.position.set(0,6,0);
+  camera.position.set(1,1,1);
   camera.name = "camera";
   scene.add(camera);
   
-  renderer.setClearColor(0x333F47, // color
-                          1); // opacity
+  
  
   //create a light
-  var light = new THREE.SpotLight(0xE8D04A);
-  light.position.set(0,200,300);
-  light.name = "light";
-  light.castShadow = true;
+  var ambient = new THREE.AmbientLight("white");
+  scene.add(ambient);
 
-  light.shadowMapWidth = 1024;
-  light.shadowMapHeight = 1024;
 
-  light.shadowCameraNear = 500;
-  light.shadowCameraFar = 4000;
-  light.shadowCameraFov = 30;
-  scene.add(light);
+  // add a spotlight to illuminate the cube and cause shadows            
+  spotLight = new THREE.SpotLight("green");
+
+  //spotLight = new THREE.DirectionalLight(0xdfebff, 1.75);
+  spotLight.name = "spotLight";
+  spotLight.position.set(4, 4, 1);
+
+  
+
+  spotLight.castShadow = true;
+  spotLight.shadowMapWidth = 512;
+  spotLight.shadowMapHeight = 512;
+  spotLight.intensity = 1;
+  spotLight.shadowDarkness = 0.1;
+  spotLight.shadowCameraNear = true;   // this line makes the shadow appear, hooray!
+
+  scene.add(spotLight);
 
   //add a cube to the scene  
-  var geometry = new THREE.BoxGeometry(1, 1, 1);
-  var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  var geometry = new THREE.CubeGeometry(0.5, 0.5, 0.5);
+  var material = new THREE.MeshPhongMaterial({ color: "yellow", ambient: "white", shininess: 9, metal: true, reflectivity: 9 });
   var cube = new THREE.Mesh(geometry, material);
   cube.name = "cube";
   cube.castShadow = true;
@@ -55,7 +65,7 @@ function init() {
   scene.add(cube);
 
   // draw a floor (plane) for the cube to sit on 
-  var planeGeometry = new THREE.PlaneBufferGeometry(20, 20);
+  var planeGeometry = new THREE.PlaneBufferGeometry(5, 5);
   var planeMaterial = new THREE.MeshPhongMaterial({ color: "yellow" });  
   var plane = new THREE.Mesh(planeGeometry, planeMaterial);
   // make the plane recieve shadow from the cube
@@ -74,8 +84,8 @@ function animate() {
   requestAnimationFrame(animate);
 
   // rotating the cube bit by bit
-  scene.getObjectByName("cube").rotation.x += 0.1;
-  scene.getObjectByName("cube").rotation.y += 0.1;
+  scene.getObjectByName("cube").rotation.x += 0.01 * 0.3;
+  scene.getObjectByName("cube").rotation.y += 0.01 * 0.5;
 
   renderer.render(scene, camera);
   controls.update();
